733dceb57662cd96f6fd8f685f66f78b35bda414
lizhenzhen <1026100702@qq.com>
2016-12-21 22:07:42 +0800

    学生和教师实体

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+
+public class Student {
+	private Integer id;
+	private String name;
+	private String sex;
+	private String address;
+	private String classz;
+	public Student(){
+		
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getClassz() {
+		return classz;
+	}
+	public void setClassz(String classz) {
+		this.classz = classz;
+	}
+}
